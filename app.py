b2c0bb69d74ea0f7da209a18ec3b4f262ee1404e
Satashee Sharma <satasheesharma@gmail.com>
2025-08-31 16:25:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -243,4 +243,5 @@ def index():
     return render_template("index.html", results=results, err=err, uploaded=preview, minScore=ui_min)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
